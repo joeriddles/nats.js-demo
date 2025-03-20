96af3f4a89879c8e9561b592165636631bfade33
Joseph Riddle <joe@synadia.com>
2025-03-20 10:01:20 -0600

    inti repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# nats.js demo
+
+Simple demo of [nat.js](https://github.com/nats-io/nats.js/) with [Synadia Cloud / NGS](https://docs.synadia.com/cloud/faq).
+
+To run:
+```shell
+npm install
+node main.mjs <nats_creds_file>
+```
+
+Expected output:
+```
+connected: connect.ngs.global:4222
+>>> hello world
+<<< hello world
+---
+connected: jetstream
+created stream: users
+created durable consumer: user-consumer
+>>> 1 data
+<<< 1 {"user_id":1}
+```
+
+Synadia Cloud — create stream:
+![Synadia Cloud stream screenshot](./static/cloud.png)
---
+++ main.mjs
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+import * as fs from "node:fs"
+import { argv, exit } from "node:process"
+
+import { credsAuthenticator } from "@nats-io/nats-core";
+import { jetstream, jetstreamManager, AckPolicy } from "@nats-io/jetstream";
+import { connect } from "@nats-io/transport-node";
+
+if (argv.length < 3) {
+  console.error('creds_file required')
+  console.error(`usage: node main.mjs <creds_file>`)
+  exit(1)
+}
+
+const [_node, _file, credsFile] = argv
+const creds = fs.readFileSync(credsFile, 'utf8');
+
+const connOpts = {
+  servers: ['nats://connect.ngs.global'],
+  // debug: true, // uncomment this for more verbose output, especially helpful when running into connection errors
+  name: 'node/main.mjs',
+  authenticator: undefined,
+};
+
+const authenticator = credsAuthenticator(new TextEncoder().encode(creds));
+connOpts.authenticator = authenticator;
+
+(async () => {
+  // Connect Core NATS
+  let nc;
+  try {
+    nc = await connect(connOpts);
+  } catch (err) {
+    console.error(`error connecting to nats: ${err.message}`);
+    return;
+  }
+  console.info(`connected: ${nc.getServer()}`);
+
+  // Test Core NATS
+  const sub = nc.subscribe('hello', {
+    callback: (err, msg) => {
+      if (err) {
+        console.error(err)
+      }
+      console.log(`<<< ${msg.subject} ${msg.data}`)
+    }
+  })
+  nc.publish('hello', 'world')
+  console.log('>>> hello world')
+  await sub.drain()
+
+  // Connect JetStream
+  console.log('---')
+
+  const js = jetstream(nc);
+  let jsm;
+  try {
+    jsm = await jetstreamManager(nc);
+  } catch (err) {
+    console.error(`error connecting to jetstream: ${err.message}`)
+  }
+  console.info('connected: jetstream')
+
+  // Create a stream
+  let streamInfo;
+  try {
+    streamInfo = await jsm.streams.add({
+      name: 'users',
+      subjects: [
+        'user-delete-account-request'
+      ],
+      max_bytes: 1_000_000, // 1MB
+    })
+  } catch (err) {
+    console.error(`failed to create stream: ${err.message}`)
+  }
+  console.info('created stream: users')
+
+  // Get a consumer
+  const getOrCreateConsumer = async() => {
+    try {
+      return await js.consumers.get(streamInfo.config.name, 'user-consumer')
+    } catch (err) {
+      if (err.name !== 'ConsumerNotFoundError') {
+        throw err
+      }
+
+      // create consumer  
+      const consumerInfo = await jsm.consumers.add(streamInfo.config.name, {
+        durable_name: "user-consumer",
+        ack_policy: AckPolicy.Explicit,
+      });
+      console.info(`created durable consumer: ${consumerInfo.config.name}`)
+      return await getOrCreateConsumer()
+    }
+  }
+  const consumer = await getOrCreateConsumer()
+
+  // Public a message to the stream
+  const data = JSON.stringify({ user_id: 1 })
+  const ack = await js.publish('user-delete-account-request', data, {
+    timeout: 10_000,
+  })
+  console.info(`>>> ${ack.seq} data`)
+
+  // Consumer message that was just published
+  const messages = await consumer.consume()
+  for await (const msg of messages) {
+    console.log(`<<< ${msg.seq} ${msg.data}`)
+    msg.ack()
+
+    if (msg.seq === ack.seq) {
+      break
+    }
+  }
+
+  // Close everything
+  await nc.close()
+  }) ()
